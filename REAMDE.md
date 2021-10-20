195e3e50eca6812c9313b8abebd55f4e28b60461
larswijnen <82577427+larswijnen@users.noreply.github.com>
2021-10-20 13:00:08 +0200

    init

---
+++ REAMDE.md
@@ -0,0 +1,22 @@
+# Generative art
+
+This repository contains some algorithms to for generative art, and some ideas.
+Will update the readme properly later.
+
+# Rocube
+
+Rendered in Blender with Cycles.
+
+![rocubes](rotubes.jpg)
+
+# Tubes
+
+Created with HTML Canvas, check `script.js` for more info.
+
+## 001
+
+![x](example1.PNG)
+
+## 002
+
+![x](example2.PNG)
---
+++ script.js
@@ -0,0 +1,111 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+randBetween = (a, b) => {
+    return () => { return Math.random() * (b-a) + a };
+}
+
+Parameters = { 
+    scale: 
+        randBetween(20, 30),
+    rotation: 
+        randBetween(0, 2*Math.PI),
+    velocity: 
+        randBetween(1, 4),
+    angularVelocity: 
+        randBetween(0, Math.PI * 0.02),
+    length: 
+        randBetween(50, 125),
+    spawn:
+        3
+}
+
+class Root {
+
+    x;
+    y;
+    scale = Parameters.scale();
+    rotX = Parameters.rotation();
+    rotY = Parameters.rotation();
+    vx = Parameters.velocity();
+    vy = Parameters.velocity();
+    rx = Parameters.angularVelocity();
+    ry = Parameters.angularVelocity();
+    length = 0;
+    maxLength = Parameters.length();
+    color;
+
+    constructor(x, y, t) {
+        this.x = x;
+        this.y = y;
+        this.color = t;
+    }
+
+    update() {
+
+        if(this.length > this.maxLength) {
+            return;
+        }
+
+        this.x += this.vx * Math.cos(this.rotX);
+        this.y += this.vy * Math.sin(this.rotY);
+        this.rotX += this.rx;
+        this.rotY += this.ry;
+
+        ctx.fillStyle = 'hsl(' + 0.1*this.color + ', 50%, ' + (this.length / this.maxLength * (50 - 20) + 30) + '%)';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.scale * (Math.cos(this.length*.1) * .5 + .8), 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.stroke();
+
+        this.length++;
+    }
+
+}
+
+const objects = [];
+let mouseDown = false;
+
+document.addEventListener("mousedown", () => {
+    mouseDown = true;
+});
+
+document.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+let mx;
+let my;
+
+document.addEventListener("mousemove", (e) => {
+    mx = e.clientX;
+    my = e.clientY;
+});
+
+let t = 0;
+var lastTick = performance.now();
+
+animate = () => {
+    requestAnimationFrame(animate);
+    var delta = performance.now() - lastTick;
+    lastTick = performance.now();
+    t += delta;
+    
+    if(mouseDown) {
+        for(let i=0; i < Parameters.spawn; i++) {
+            const root = new Root(mx, my, t);
+            objects.push(root);
+        }
+    }
+
+    for(let object of objects) {
+        object.update();
+    }
+};
+
+animate();
+
